255262d4b0aa489782b3541e28989b256aa1cf58
ozantalay <ozntalay@hotmail.com>
2024-07-18 15:49:09 +0300

    problem_01_final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import Form from './Form'
+import Header from './Header'
+
+
+// App/Header/Form bileşenlerine ayırın, kendi adlarıyla ayrı dosyalara koyun
+
+export default function App() {
+  return (
+    <div>
+    <Header/>
+      <Form/>
+    </div>
+
+      
+    
+  )
+}
